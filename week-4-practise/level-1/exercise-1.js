a85a372cae6aa5122357f7b915dbe6485367b6e1
Francis <kabuye_frank@yahoo.com>
2018-06-03 12:24:46 +0100

    more week-4

--- week-4-practise/level-1/exercise-1.js
+++ week-4-practise/level-1/exercise-1.js
@@ -39,6 +39,17 @@ var writers = [
   },
 ];
 
+var famousWriters = writers.forEach(function(writer) {
+  console.log(`Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old, and work as a ${writer.occupation}.`);
+})
 /*
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
+var writersAlive = writers.filter(function(writer) { 
+  if(writer.alive === true) {
+    return `Hi, my name is ${writer.firstName} ${writer.lastName}.`;
+     }else{
+    return false;
+     }
+  })
+  console.log(writersAlive);
